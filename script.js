5aa0ead8561293cbea60519262b1bcff3b554cc2
Karlos Messyas <karlosmessyas14@gmail.com>
2024-04-02 18:52:38 -0300

    Adicionando configurações inicias do leaflet

--- script.js
+++ script.js
@@ -8,3 +8,31 @@ buttonTheme.addEventListener("click", () => {
         html.setAttribute('data-bs-theme', 'dark');
     }
 });
+
+var map = L.map('map', {
+    center: [-6.844181, -38.348011],
+    zoom: 15,
+    minZoom: 14,
+    maxZoom: 16
+});
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+map.locate();
+
+map.on('locationfound', e=>{
+    marker.setLatLng(e.latlng);
+    map.setView(e.latlng);
+});
+
+map.on('click', l => {
+    marker.setLatLng(l.latlng);
+    map.setView(l.latlng);
+})
+
+var marker = L.marker([-6.844181, -38.348011], {
+    draggable: true
+}).addTo(map);
